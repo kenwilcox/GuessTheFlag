8540f21f9ccc1f1a68b0e84c4171449651cdec2c
Ken Wilcox <wilcoxk@gmail.com>
2015-10-15 23:38:39 -0600

    Loading the buttons with random images, picking a random country

--- GuessTheFlag/ViewController.swift
+++ GuessTheFlag/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GameplayKit
 
 class ViewController: UIViewController {
   
@@ -14,9 +15,25 @@ class ViewController: UIViewController {
   @IBOutlet weak var button2: UIButton!
   @IBOutlet weak var button3: UIButton!
   
+  var countries = [String]()
+  var correctAnswer = 0
+  var score = 0
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+    
+    // Flags with white "disapear" a border makes them show up properly
+    button1.layer.borderWidth = 1
+    button2.layer.borderWidth = 1
+    button3.layer.borderWidth = 1
+    
+    button1.layer.borderColor = UIColor.lightGrayColor().CGColor
+    button2.layer.borderColor = UIColor.lightGrayColor().CGColor
+    button3.layer.borderColor = UIColor.lightGrayColor().CGColor
+    
+    askQuestion()
   }
   
   override func didReceiveMemoryWarning() {
@@ -24,6 +41,17 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
   
-  
+  func askQuestion() {
+    // GameplayKit random suffle
+    countries = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(countries) as! [String]
+    
+    button1.setImage(UIImage(named: countries[0]), forState: .Normal)
+    button2.setImage(UIImage(named: countries[1]), forState: .Normal)
+    button3.setImage(UIImage(named: countries[2]), forState: .Normal)
+    
+    // Get a random number - either 0, 1, 2
+    correctAnswer = GKRandomSource.sharedRandom().nextIntWithUpperBound(3)
+    title = countries[correctAnswer].uppercaseString
+  }
 }
 
